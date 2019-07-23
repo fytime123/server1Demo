b4036993e61eea613ceb8de64a9636e0bd32c213
liufuyi <109335992@qq.com>
2019-07-23 08:00:15 +0800

    learning sources

--- src/main/java/com/liufuyi/demo/DemoApplication.java
+++ src/main/java/com/liufuyi/demo/DemoApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoApplication {
 
+    //https://www.cnblogs.com/wmyskxz/p/9010832.html
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
